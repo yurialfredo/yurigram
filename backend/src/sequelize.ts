306fe6aed5966c6380cdc633e504b67a3ae73b8d
gasparin <yurialfredo@gmail.com>
2019-12-21 18:04:37 -0600

    initial version

---
+++ backend/src/sequelize.ts
@@ -0,0 +1,19 @@
+import {Sequelize} from 'sequelize-typescript';
+import { config } from './config/config';
+
+
+const c = config.prod;
+
+// Instantiate new Sequelize instance!
+export const sequelize = new Sequelize({
+  "username": c.username,
+  "password": c.password,
+  "database": c.database,
+  "host":     c.host,
+
+  dialect: 'postgres',
+  storage: ':memory:',
+});
+
+
+console.log(`sequelize: ${JSON.stringify(c)}`)
